3fdf07bc937debe1a4f044daef376182686d6e8a
Jordan Powers <jordanpowers1227@gmail.com>
2022-02-21 13:55:24 -0800

    albumart: Support flac files

--- albumart/README.md
+++ albumart/README.md
@@ -1,11 +1,11 @@
 # Add Albumart
 
 ## Description
-This script adds album art to mp3 files.
+This script adds album art to mp3 or flac files.
 
 ## Inputs
-- `in/*.mp3` Coverless mp3 files
+- `in/*.{mp3,flac}` Coverless mp3 or flac files
 - `in/cover.{png,jpg}` Album art
 
 ## Outputs
-- `out/*.mp3` Covered mp3 files
+- `out/*.{mp3,flac}` Covered mp3 or flac files
--- albumart/add_albumart.py
+++ albumart/add_albumart.py
@@ -1,11 +1,23 @@
 from pathlib import Path
 import subprocess
 import shutil
+import itertools
 
 # Helpful: https://www.covermytunes.com/
 # Potential future project: automatically pull covers based on metadata
 
 script_dir = Path(__file__).resolve().parent
+
+ffmpeg_options_map = {
+    '.mp3': [
+        "-id3v2_version", "3",
+        "-metadata:s:v", "title=cover",
+        "-metadata:s:v", "comment=Cover (front)"
+    ],
+    '.flac': [
+        "-disposition:1", "attached_pic"
+    ]
+}
 
 indir = script_dir / 'in'
 outdir = script_dir / 'out'
@@ -30,19 +42,18 @@ if outdir.exists():
 
 outdir.mkdir()
 
-for mp3path in indir.glob("*.mp3"):
-    outpath = outdir / mp3path.name
+globs = [indir.glob(f"*{ext}") for ext in ffmpeg_options_map.keys()]
+
+for inpath in itertools.chain.from_iterable(globs):
+    outpath = outdir / inpath.name
+    ffmpeg_opts = ffmpeg_options_map[inpath.suffix]
     completion = subprocess.run(['ffmpeg', 
-            '-i', mp3path,
+            '-i', inpath,
             '-i', coverpath,
             '-map', '0:a',
             '-map', '1:v',
-            '-map_metadata', '0',
-            '-acodec', 'copy',
-            '-vcodec', 'copy',
-            "-id3v2_version", "3",
-            "-metadata:s:v", "title=cover",
-            "-metadata:s:v", "comment=Cover (front)",
+            '-map_metadata', '0'
+            ] + ffmpeg_opts + [
             outpath
             ])
     if completion.returncode != 0:
