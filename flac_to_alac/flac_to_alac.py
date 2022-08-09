ec55560af6b94d46c326c9178387dca9a17e0d6c
Jordan Powers <jordanpowers1227@gmail.com>
2022-08-08 18:15:56 -0700

    flac_to_alac: Support modifying metadata

--- flac_to_alac/flac_to_alac.py
+++ flac_to_alac/flac_to_alac.py
@@ -1,26 +1,70 @@
+import argparse
 from pathlib import Path
 import subprocess
 import shutil
 
 script_dir = Path(__file__).resolve().parent
 
-indir = script_dir / 'in'
-outdir = script_dir / 'out'
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--in', default=str(script_dir / 'in'), dest='indir')
+parser.add_argument('--out', default=str(script_dir / 'out'), dest='outdir')
+parser.add_argument('--cover')
+parser.add_argument('--number', action='store_true')
+parser.add_argument('--album')
+
+args = parser.parse_args()
+
+indir = Path(args.indir)
+outdir = Path(args.outdir)
+
+coverpath = None
+if args.cover:
+    coverpath = Path(args.cover)
+    if not coverpath.is_file():
+        coverpath = None
+
 
 if outdir.exists():
     shutil.rmtree(outdir)
 
 outdir.mkdir()
 
-for inpath in indir.glob("*.flac"):
-    outpath = outdir / f"{inpath.stem}.m4a"
-    completion = subprocess.run(['ffmpeg', 
-            '-i', inpath,
+infiles = list(indir.glob("*.flac"))
+
+def get_ffmpeg_invocation(i, inpath, outpath, coverpath):
+    metadata = []
+    if args.number:
+        metadata += [ '-metadata', f'track={i+1}']
+    if args.album:
+        metadata += [ '-metadata', f'album={args.album}' ]
+    if coverpath is None:
+        return [
+            'ffmpeg',
+            '-i', str(inpath),
             '-acodec', 'alac',
             '-vcodec', 'copy',
-            '-disposition:v', 'attached_pic',
+            '-disposition:v', 'attached_pic'
+            ] + metadata + [
             str(outpath)
-            ])
+        ]
+    else:
+        return [
+            'ffmpeg',
+            '-i', str(inpath),
+            '-i', str(coverpath),
+            '-map', '0:a',
+            '-map', '1:v',
+            '-acodec', 'alac',
+            '-vcodec', 'copy',
+            '-disposition:v', 'attached_pic'
+            ] + metadata + [
+            str(outpath)
+        ]
+
+for i, inpath in enumerate(infiles):
+    outpath = outdir / f"{inpath.stem}.m4a"
+    completion = subprocess.run(get_ffmpeg_invocation(i, inpath, outpath, coverpath))
     if completion.returncode != 0:
         print("ffmpeg error")
         exit(1)
